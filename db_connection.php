0a398d95706df447057e3aebf69b09d33dbe00f1
AnwarNaji24 <anwar05099@gmail.com>
2025-12-17 22:41:43 +0300

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -3,27 +3,27 @@ $host = "localhost";
 $username = "root";
 $password = "";
 
+//Connect to the MySQL server
 $conn = mysqli_connect($host, $username, $password);
 
-if (!$conn) {
-    die("Connection failed: ". mysqli_connect_error());
-}
-
-$sql = 'CREATE Database users_db';
-mysqli_query($conn, $sql);
-mysqli_select_db($conn, "users_db" );
-
+//Check if connection was successful
 /*if(!$conn){
     echo mysqli_connect_error();
 }*/
 
+// Create the 'users_db' database
+$sql = 'CREATE Database users_db';
+mysqli_query($conn, $sql);
+mysqli_select_db($conn, "users_db" );
+
+//Create the 'Users' table
 $sql = 'CREATE TABLE Users('.
     'UserID INT AUTO_INCREMENT PRIMARY KEY,'.
     'Username VARCHAR(50) NOT NULL UNIQUE,'.
     'Password VARCHAR(255) NOT NULL,'.
     'Email VARCHAR(100) NOT NULL UNIQUE)';
 
-$res = mysqli_query($conn, $sql);
+$res = mysqli_query($conn, $sql); //Execute the query
 
 /*if($res === TRUE){
     echo "yes";
@@ -32,6 +32,7 @@ else{
     echo mysqli_error($conn);
 }*/
 
+//Create the 'Quizzes' table
 $sql = 'CREATE TABLE Quizzes('.
     'QuizID INT AUTO_INCREMENT PRIMARY KEY,'.
     'UserID INT NOT NULL,'.
@@ -39,7 +40,7 @@ $sql = 'CREATE TABLE Quizzes('.
     'FOREIGN KEY (UserID) REFERENCES Users(UserID)'.
     'ON DELETE CASCADE)';
 
-$res2 = mysqli_query($conn, $sql);
+$res2 = mysqli_query($conn, $sql); // Execute the query
 
 /*if($res2 === TRUE){
     echo "yes";
@@ -49,4 +50,3 @@ else{
 }*/
 
 ?>
-
